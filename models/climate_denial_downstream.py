20996cd5ada71872c8ba93797f6d67d3af8e87e5
haleyej <haleyej@umich.edu>
2024-04-09 13:58:46 -0400

    improving eval

--- models/climate_denial_downstream.py
+++ models/climate_denial_downstream.py
@@ -1,12 +1,15 @@
 import os 
 import torch
+import evaluate
 import argparse
+import numpy as np
 import pandas as pd
 
 from torch.utils.data import Dataset
 from sklearn.model_selection import train_test_split
 from transformers import DataCollatorWithPadding, RobertaTokenizerFast, AutoModelForSequenceClassification, TrainingArguments, Trainer
 
+metrics = evaluate.combine(["accuracy", "f1", "precision", "recall"])
 
 def process_climate_data(path:str) -> tuple:
     '''
@@ -17,6 +20,12 @@ def process_climate_data(path:str) -> tuple:
     labels = df['sentiment'].to_list()
     return texts, labels 
 
+
+def compute_metrics(pred):
+    print('COMPUTING METRICS!')
+    logits, labels = pred
+    predictions = np.argmax(logits, axis=-1)
+    return metrics.compute(predictions=predictions, references=labels)
 
 
 class ClimateClassificationDataset(Dataset):
@@ -61,14 +70,16 @@ def classification_pretrain(tokenizer,
                             eval_size:int = 0.2, 
                             max_len:int = 512,
                             batch_size:int = 8,
+                            eval_steps:int = 1000,
                             learning_rate = 1e-5, 
                             epochs:int = 5, 
                             weight_decay:float = 0.01) -> None:
     
     texts, labels = process_climate_data(data_path)
     X_train, X_val, y_train, y_val = train_test_split(texts, labels, test_size = eval_size)
+    num_classes = len(set(labels))
 
-    model = AutoModelForSequenceClassification.from_pretrained(pretrained_model_path)   
+    model = AutoModelForSequenceClassification.from_pretrained(pretrained_model_path, num_labels = num_classes)   
 
     climate_dataset = ClimateClassificationDataset(X_train, y_train, tokenizer, max_len)
     eval_dataset = ClimateClassificationDataset(X_val, y_val, tokenizer, max_len)
@@ -80,31 +91,34 @@ def classification_pretrain(tokenizer,
         num_train_epochs = epochs,
         per_device_train_batch_size = batch_size,
         per_device_eval_batch_size = batch_size,
-        evaluation_strategy = "epoch",
+        evaluation_strategy = "steps",
+        eval_steps = eval_steps,
         logging_dir = logging_dir,
         logging_strategy = "steps",
         logging_steps = 50,
         learning_rate = learning_rate,
         weight_decay = weight_decay,
         warmup_steps = 500,
-        save_strategy = "epoch",
+        save_strategy = "steps",
         load_best_model_at_end = True)
-    
+
     trainer = Trainer(
             model = model,
             args = training_args,
             train_dataset = climate_dataset,
             eval_dataset = eval_dataset,
             data_collator = data_collator,
-            tokenizer = tokenizer)
+            tokenizer = tokenizer, 
+            compute_metrics = compute_metrics)
 
     trainer.train()
-    torch.save(model.state_dict(), f"{model_name}-weights.pth")
-
-
+    torch.save(model.state_dict(), f"{model_name}-classification-weights.pth")
 
 
 def main(args):
+    # how to run from command line 
+    # python3 climate_denial_downstream.py --pretrained_model_path='reddit-left' --data_path='../data/climate_sentiment_test.csv' 
+    # replace paths with the correct ones for your dir structure
     tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base', do_lower_case = True)
 
     classification_pretrain(tokenizer,
@@ -116,6 +130,7 @@ def main(args):
                             args['eval_size'], 
                             args['max_len'],
                             args['batch_size'],
+                            args['eval_steps'],
                             args['learning_rate'],
                             args['epochs'], 
                             args['weight_decay'])
@@ -132,6 +147,7 @@ def parse_args():
     parser.add_argument('--eval_size', type=int, default=0.2)
     parser.add_argument('--max_len', type=int, default=512)
     parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--eval_steps', type=int, default=50)
     parser.add_argument('--learning_rate', type=float, default=1e-5)
     parser.add_argument('--epochs', type=int, default=5)
     parser.add_argument('--weight_decay', type=float, default=0.01)
